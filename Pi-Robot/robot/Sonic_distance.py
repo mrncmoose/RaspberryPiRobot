fe59d66e0376b12ea02d336f965d1e2dbe2502fd
fred.dunaway <fred.dunaway@cohnreznick.com>
2019-07-06 10:42:06 -0400

    Adding base class for sonic distance sensors

---
+++ Pi-Robot/robot/Sonic_distance.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 6, 2019
+
+@author: moose
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
