e97b8cfb2d246cd5a83f24a7c50b11c868b2c69a
fred.dunaway <fred.dunaway@cohnreznick.com>
2019-10-15 10:09:14 -0400

    Updating readme and adding sonic distance class.

--- Pi-Robot/robot/Sonic_distance.py
+++ Pi-Robot/robot/Sonic_distance.py
@@ -1,8 +1,11 @@
 '''
 Created on Jul 6, 2019
 
-@author: moose
+@author: Mr NC Moose.  A.K.A. Fred T. Dunaway
 '''
+import RPi.GPIO as GPIO
+import time
+
 
 class MyClass(object):
     '''
@@ -10,8 +13,49 @@ class MyClass(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, TriggerPin = 4, EchoPin = 17, Tolerance = 0.1):
         '''
         Constructor
-        '''
-        
+        ''' 
+        #GPIO Mode (BOARD / BCM)
+        GPIO.setmode(GPIO.BCM)
+        #set GPIO direction (IN / OUT)
+        GPIO.setup(TriggerPin, GPIO.OUT)
+        GPIO.setup(EchoPin, GPIO.IN)
+        self.tolerance = Tolerance
+
+    def GetDistance(self):
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+     
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
+     
+        StartTime = time.time()
+        StopTime = time.time()
+     
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+     
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+     
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+     
+        return distance
+
+    
+    def AtDistance(self, distance, tolerance = 0.1):
+        currentDistance = self.GetDistance()
+        if currentDistance <= distance - tolerance and currentDistance >= distance + tolerance:
+            return True
+        return False
+    
+            
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Raspberry Pi Robot
-The objective of this project is to learn how to make a robot that will react to things around it and to keep a specific distance from them.  First and formost, it is a learning experiance.  There are other projects out there that do the same basic thing.  Very easy to copy, but little to nothing will be learned from just cloning a project.
+The objective of this project is to learn how to make a robot that will react to things around it and to keep a specific distance from them.  First and foremost, it is a learning experience.  There are other projects out there that do the same basic thing.  Very easy to copy, but little to nothing will be learned from just cloning a project.
 
 This project starts with a blank sheet of paper and few ideas on how to get started.  It is open ended because the project is for fun and learning.
+
+Current work is to create base classes that will handle distance functions, movement functions and rules for when to move and how much based on distance sensed.
